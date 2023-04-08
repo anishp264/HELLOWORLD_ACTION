78dde9621f456fb3f02b607bcb7d0a8b49368c22
anishp264 <punnenanish@gmail.com>
2023-04-08 21:20:22 +0200

    Hello world action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+try{
+    //If an error, then the below lines of code will not execute. But the github action still shows as success
+    //Add the core.SetFailed to mark the github action as a failure
+    //throw(new Error("some error msg"));
+
+    const name = core.getInput('who-to-greet');
+    console.log(`Hello ${name}`);
+
+    const time = new Date();
+    core.setOutput(time);
+
+    //console.log(JSON.stringify(github, null, '\t'));
+}
+catch(error){
+    core.setFailed(error.message);
+}
