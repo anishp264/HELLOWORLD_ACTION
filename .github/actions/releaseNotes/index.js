813dc3c9c5692b7966a1946c05fbffde4fd83255
anishp264 <punnenanish@gmail.com>
2023-06-05 15:42:01 +0200

    Create github action to get commit message

--- .github/actions/releaseNotes/index.js
+++ .github/actions/releaseNotes/index.js
@@ -2,17 +2,31 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 
-try{
+const token = core.getInput("token");
+const owner = core.getInput("owner");
+const repo = core.getInput("repo");
+const sha = core.getInput("sha");
 
-    const owner = core.getInput("owner");
-    const repo = core.getInput("repo");
-    const sha = core.getInput("sha");
+core.setOutput("owner", owner);
+core.setOutput("repo", repo);
+core.setOutput("sha", sha);
 
-    core.setOutput("owner", owner);
-    core.setOutput("repo", repo);
-    core.setOutput("sha", sha);
-
+async function getCommitMessage() {
+    const octokit = new Octokit(token);
+  
+    try {
+      const commit = await octokit.repos.getCommit({
+        owner,
+        repo,
+        ref: sha
+      });
+  
+      const commitMessage = commit.data.commit.message;
+      return commitMessage;
+    } catch (error) {
+        core.setFailed(error.message);
+      return null;
+    }
 }
-catch(error){
-    core.setFailed(error.message);
-}
+
+core.setOutput("commitMessage", getCommitMessage());
