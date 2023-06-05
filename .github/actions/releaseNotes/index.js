7682b3a25d8817575ef85c6e5f9eb75e3540d8a9
anishp264 <punnenanish@gmail.com>
2023-06-05 17:31:49 +0200

    Create github action to get commit message -24

--- .github/actions/releaseNotes/index.js
+++ .github/actions/releaseNotes/index.js
@@ -6,7 +6,8 @@ const token = core.getInput("token");
 const owner = core.getInput("owner");
 const repoOwner = core.getInput("repo");
 const ref = core.getInput("sha");
-const repo  = repoOwner.split("/")[1];
+const repo  = "HELLOWORLD_ACTION";
+//repoOwner.split("/")[1];
 
 async function getCommitMessage() {
     const octokit = new Octokit({
