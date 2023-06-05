17b1fbb21097cd7526ac6018ba7cb910feccd231
anishp264 <punnenanish@gmail.com>
2023-06-05 17:00:55 +0200

    Create github action to get commit message -18

--- .github/actions/releaseNotes/index.js
+++ .github/actions/releaseNotes/index.js
@@ -26,9 +26,10 @@ async function getCommitMessage() {
     }
 }
 
-const msg = getCommitMessage();
-
-core.setOutput("commitMessage", msg);
 core.setOutput("owner", owner);
 core.setOutput("repo", repo);
 core.setOutput("sha", ref);
+
+const msg = getCommitMessage();
+
+core.setOutput("commitMessage", msg);
